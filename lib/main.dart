be45c278b2e746850fa837cd27fc542446845a9a
abobaker <162491153+abobakerer@users.noreply.github.com>
2025-05-07 02:44:08 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/shopping_item.dart';
+import 'package:flutter_application_1/shopping_listPage.dart';
+import 'package:hive_flutter/adapters.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(ShoppingItemAdapter());
+  Hive.registerAdapter(ShoppingItemAdapter());
+  await Hive.openBox<ShoppingItem>('shopping_items');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping List',
+      theme: ThemeData.dark(),
+      home: const ShoppingListPage(),
+    );
+  }
+}
